30c40e3de408ec3ecfdfca6e506b03d158baac9e
canuzundemir <61251350+canuzundemir@users.noreply.github.com>
2021-01-04 15:14:39 +0300

    Update dnbr.js

--- dnbr.js
+++ dnbr.js
@@ -23,7 +23,7 @@ function stretch(val, min, max) {return (val - min) / (max - min);}
 function filterScenes (scenes, inputMetadata) {  
 return scenes.filter(function (scene) {
 // set dates for pre-and-post fire analysis
-var allowedDates = ["2020-08-13","2020-10-07"]; // Knysna fires
+var allowedDates =  ["2020-08-23","2020-10-07"]; // Knysna fires
 // format scene date timestamp to match allowed dates 
 var sceneDateStr = dateformat(scene.date);
 if (allowedDates.indexOf(sceneDateStr)!= -1) return true;
